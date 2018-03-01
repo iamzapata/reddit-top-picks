fb5c4473404fec5941579bd9438e6de9f0a4f6f1
Andres Zapata <me@azapata.io>
2018-02-28 22:11:33 -0500

    Add redux-logger to project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware, combineReducers } from 'redux'
-import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import logger from 'redux-logger'
 import topics from 'store/reducers/reducers'
 import App from 'App'
 import './index.css'
 
-const store = createStore(combineReducers({topics}), applyMiddleware(thunk))
+const middleware = [thunk, logger]
+
+const store = createStore(combineReducers({topics}), applyMiddleware(...middleware))
 
 ReactDOM.render(
   <Provider store={store}>
